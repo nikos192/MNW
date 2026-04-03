37d09d477b9e5e0bf0a61653f1f3d63c77c3be36
nikos192 <nikos_192837@outlook.com>
2026-04-03 23:05:21 +1100

    Update Instagram profile link

--- frontend/src/lib/brand.ts
+++ frontend/src/lib/brand.ts
@@ -1,5 +1,5 @@
 export const BRAND_NAME = "MonzaWheels";
 export const BRAND_LEGAL_NAME = "MonzaWheels and Accessories";
 export const BRAND_EMAIL = "admin@monzawheel.com";
-export const BRAND_INSTAGRAM_URL = "https://instagram.com/monzawheels";
+export const BRAND_INSTAGRAM_URL = "https://www.instagram.com/monza_wheels?igsh=N24yeWhudjFsZHg5&utm_source=qr";
 export const BRAND_FACEBOOK_URL = "https://facebook.com/monzawheels";
